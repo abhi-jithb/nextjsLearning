431f237b057954ca6e140b4df32d5a3b370aedbf
Abhijith B <babhijith58@gmail.com>
2026-02-02 22:42:56 +0530

    explore: routing

---
+++ app/users/new/page.tsx
@@ -0,0 +1,10 @@
+
+import React from 'react'
+
+const newUsersPage = () => {
+  return (
+    <div>newUsersPage</div>
+  )
+}
+
+export default newUsersPage
---
+++ app/users/page.tsx
@@ -0,0 +1,11 @@
+// Next.js looks for conventions over configuration
+
+import React from 'react'
+
+const UsersPage = () => {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
